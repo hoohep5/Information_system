a8ac3e21a994726c7ada53b33c3723a5ce27c7a5
Vorin2473 <viktorgotovcev66@gmail.com>
2023-04-18 14:02:11 +1000

    Ocnovnaia mekanika

--- Procedure.py
+++ Procedure.py
@@ -1,21 +1,38 @@
 class Procedure():
-    def __init__(self, id):
+    def __init__(self, id, data, name, description, price):
         self.id = id
-    id = 0
+        self.data = data
+        self.name = name
+        self.description = description
+        self.price = price
+    id = "0"
     data = 0
     name = "noname"
     description = "text"
     price = 0
     master_id = 0
     def createNewProcedure(self):
-        pass
+        f = open("Procedure.txt", "a+")
+        lines = f.readlines()
+        for l in lines:
+            if l != str(self.id)+"-"+str(self.data)+"-"+str(self.name)+"-"+str(self.description)+"-"+str(self.price)+"\n":
+                f.write(str(self.id)+"-"+str(self.data)+"-"+str(self.name)+"-"+str(self.description)+"-"+str(self.price)+"\n")
+            else:
+                pass
+        f.close()
     def getAllProcedures(self):
-        pass
+        f = open("Procedure.txt", "r")
+        lines = f.readlines()
+        for l in lines:
+            print(l.strip())
+        f.close()
     def getAllSorted(self):
         pass
-    def getProcedureById(self, id):
-        if self.id == id:
-            print(self.description)
+    def getProcedureById(self, list ,id):
+        for i in range(len(list)):
+            if list[i].id == id:
+                print(list[i].description + "\nprice: " + str(list[i].price))
     def deleteProcedure(self):
         pass
-    pass
+    def checkProcedures(self):
+        pass
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import Procedure as p
-a = p.Procedure(1)
-a.getProcedureById(a.id)
+fProc = open("Procedure.txt", "r")
+procedures = fProc.readlines()
+for line in range(len(procedures)):
+    procedures[line] = procedures[line].replace("\n", "")
+    procedures[line] = procedures[line].split("-")
+for line in range(len(procedures)):
+    print(procedures[line])
+proceduresEcz = []
+for line in range(len(procedures)):
+    proceduresEcz.append(p.Procedure(procedures[line][0],procedures[line][1],procedures[line][2],procedures[line][3],procedures[line][4]))
+for line in range(len(proceduresEcz)):
+    print(proceduresEcz[line].id)
