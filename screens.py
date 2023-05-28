7d3b68362c98c6cbd48337fa2d3bdaba8ecac2dd
Oleg <Metallov2004@yandex.ru>
2023-05-28 17:01:13 +1000

    screens

--- screens.py
+++ screens.py
@@ -37,7 +37,8 @@
 #         os.system('cls')
 #         print(sign_up)
 #         time.sleep(2)
-
+procedures = ["massageyrtyrty", "z", "pilling", "massageyrtyrty", "z", "pilling"]
+salons = ["svenlanskaya", "aleutskaya", "komsomolcskaya"]
 
 class Window:
     # def __init__(self, frames, object1, object2, object3):
@@ -46,16 +47,51 @@ class Window:
     #     self.object2 = object2
     #     self.object3 = object3
     @staticmethod
-    def printframes(length, width, back):
-        print("╭", "─"*(length-2), "╮", sep = "")
+    def printBasicWindow(length, width, back, title, list):
+        print("╭", "─"*(length - 2), "╮", sep = "")
+        isTitled = False
         if back:
-            print("│", " " * (length - 8), "┌────┐", "│", sep = "")
+            if (length % 2 == 0 and len(title) % 2 == 0) or (length % 2 != 0 and len(title) % 2 != 0):
+                print("│", " " * ((length - len(title) - 1) // 2), title, " " * ((length - len(title) - 1) // 2 - 6), "┌────┐", "│", sep = "")
+            else:
+                print("│", " " * ((length - len(title) - 1) // 2), title, " " * ((length - len(title) - 1) // 2 - 7), "┌────┐", "│", sep = "")
             print("│", " " * (length - 8), "│Back│", "│", sep = "")
             print("│", " " * (length - 8), "└────┘", "│", sep = "")
             width -= 3
-        for i in range(width-1, 1, -1):
-            print("│", " " * (length - 2), "│", sep = "")
+            isTitled = True
+        if isTitled:
+            # for i in range(width - 1, 1, -1):
+            #     print("│", " " * (length - 2), "│", sep = "")
+            for j in list:
+                print("│", j, " " * ((length - 5) - len(j)), "│")
+        else:
+            if (length % 2 == 0 and len(title) % 2 == 0) or (length % 2 != 0 and len(title) % 2 != 0):
+                print("│", " " * ((length - len(title) - 1) // 2), title, " " * ((length - len(title) - 1) // 2), "│", sep = "")
+            else:
+                print("│", " " * ((length - len(title) - 1) // 2), title, " " * ((length - len(title) - 1) // 2 - 1), "│", sep = "")
+            for j in list:
+                print("│", j, " " * ((length - 5) - len(j)), "│")
+            # for i in range(width - 2, 1, -1):
+            #     print("│", " " * (length - 2), "│", sep = "")
         print("╰", "─" * (length - 2), "╯", sep = "")
 
+    def printSignIn(length, width):
+        print("╭", "─" * (length - 2), "╮", sep = "")
+        for i in range((width // 2) - 1, 1, -1):
+            print("│", " " * (length - 2), "│", sep = "")
+        if length % 2 == 0:
+            print("│", " " * ((length - 14) // 2), "Введите логин", " " * ((length - 14) // 2 - 1), "│", sep = "")
+            print("│", " " * ((length - 9) // 2), "и пароль", " " * ((length - 9)//2), "│", sep = "")
+        else:
+            print("│", " " * ((length - 14) // 2), "Введите логин", " " * ((length - 14) // 2), "│", sep = "")
+            print("│", " " * ((length - 9) // 2), "и пароль", " " * ((length - 9) // 2 - 1), "│", sep = "")
+        for i in range((width // 2) - 1, 1, -1):
+            print("│", " " * (length - 2), "│", sep = "")
+        print("╰", "─" * (length - 2), "╯", sep = "")
+        login = input()
+        password = input()
 
-Window.printframes(25, 10, False)
+
+Window.printBasicWindow(35, 3, False, "Добро пожаловать!", salons)
+Window.printBasicWindow(35, 3, False, "Вы зарегистрировались", salons)
+Window.printBasicWindow(40, 10, False, "Выберите процедуру", procedures)
